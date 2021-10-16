65dbc078cd2e6418631ddcd8b358dd93405e547a
Prooheckcp <vasco.soares.2001@gmail.com>
2021-10-16 18:08:58 +0100

    Basic structure

---
+++ wordgame.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//Namespaces||
+using namespace std;
+//__________||
+
+int main(int argc, char const *argv[])
+{
+    
+    std::cout << "Hello wordl!" << std::endl;
+    return 0;
+}
